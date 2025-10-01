fbcae485d58c1b36f7959fb3d2e5c638923da949
superstan777 <viir4d@gmail.com>
2025-10-01 21:28:21 +0200

    feat: add tabs for selecting visible data

--- src/app/(protected)/users/[id]/page.tsx
+++ src/app/(protected)/users/[id]/page.tsx
@@ -1,8 +1,9 @@
+// src/app/(protected)/users/[id]/page.tsx
 import { getUser } from "@/lib/api/users";
 import { UserForm } from "@/components/UsersPage/UserForm";
-
 import { UserDevicesList } from "@/components/UsersPage/UserDevicesList";
 import { UserTicketsList } from "@/components/UsersPage/UserTicketsList";
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
 
 interface UserPageProps {
   params: { id: string };
@@ -19,13 +20,46 @@ export default async function UserPage({ params }: UserPageProps) {
     return <div>User not found.</div>;
   }
 
+  const triggerClass =
+    "px-4 py-2 rounded-t-md border-b-2 border-transparent " +
+    "data-[state=active]:border-b-gray-200 data-[state=active]:-mb-px " +
+    "data-[state=active]:text-gray-900";
+
   return (
     <div className="p-4">
       <UserForm user={user} />
 
-      <UserDevicesList userId={id} deviceType="computer" />
-      <UserDevicesList userId={id} deviceType="monitor" />
-      <UserTicketsList userId={id} />
+      <div className="mt-6 w-full">
+        <Tabs defaultValue="computers" className="w-full">
+          <div className="flex justify-center ">
+            <TabsList className="inline-flex mb-0 space-x-4">
+              <TabsTrigger value="computers" className={triggerClass}>
+                Computers
+              </TabsTrigger>
+              <TabsTrigger value="monitors" className={triggerClass}>
+                Monitors
+              </TabsTrigger>
+              <TabsTrigger value="tickets" className={triggerClass}>
+                Tickets
+              </TabsTrigger>
+            </TabsList>
+          </div>
+
+          <div>
+            <TabsContent value="computers">
+              <UserDevicesList userId={id} deviceType="computer" />
+            </TabsContent>
+
+            <TabsContent value="monitors">
+              <UserDevicesList userId={id} deviceType="monitor" />
+            </TabsContent>
+
+            <TabsContent value="tickets">
+              <UserTicketsList userId={id} />
+            </TabsContent>
+          </div>
+        </Tabs>
+      </div>
     </div>
   );
 }

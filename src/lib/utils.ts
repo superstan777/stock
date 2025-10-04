782756fcfed4d08e8f19faeb76ca08c48493958b
superstan777 <viir4d@gmail.com>
2025-10-04 19:55:05 +0200

    fix: update format label function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,9 +7,9 @@ export function cn(...inputs: ClassValue[]) {
 
 export function formatLabel(key: string): string {
   return key
-    .split("_")
+    .split(/[_\.]/)
     .map((word) => {
-      if (word.toLowerCase() === "id") return "ID"; // special case
+      if (word.toLowerCase() === "id") return "ID"; // specjalny przypadek
       return word.charAt(0).toUpperCase() + word.slice(1);
     })
     .join(" ");
